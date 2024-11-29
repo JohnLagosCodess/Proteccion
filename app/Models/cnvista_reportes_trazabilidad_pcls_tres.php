e47f44e6d0ae48603d4ad699e7d250a185041a9c
Luis Gomes <luis.gomez@codess.org.co>
2024-11-29 12:12:48 -0500

    actualización

---
+++ app/Models/cnvista_reportes_trazabilidad_pcls_tres.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class cnvista_reportes_trazabilidad_pcls_tres extends Model
+{
+    use HasFactory;
+}
